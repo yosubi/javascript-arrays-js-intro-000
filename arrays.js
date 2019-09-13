2d69802a0e781d434838212a557d10bff5575bce
Joseph Moon <nakkta@gmail.com>
2019-09-13 20:50:43 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -34,7 +34,7 @@ function removeElementFromBeginningOfArray(array) {
   return array.slice(1)
 }
 
-function arrays destructivelyRemoveElementFromEndOfArray(array) {
-  array.pop()
+function destructivelyRemoveElementFromEndOfArray(array) {
+  array.pop
   return array
 }
