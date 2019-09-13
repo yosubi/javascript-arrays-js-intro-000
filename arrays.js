27e538c5500abfa1ea4b3ee8439abc201544ef10
Joseph Moon <nakkta@gmail.com>
2019-09-13 20:46:18 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -24,3 +24,8 @@ function destructivelyAddElementToEndOfArray(array, element) {
 function accessElementInArray(array, index) {
   return array[index]
 }
+
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+  array.shift()
+  return array
+}
